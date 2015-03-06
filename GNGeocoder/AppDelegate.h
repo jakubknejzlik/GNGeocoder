b7020bd6c0e7e47449e600cc0fad544d5cec77e6
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2015-03-06 10:05:35 +0100

    Initial Commit

---
+++ GNGeocoder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GNGeocoder
+//
+//  Created by Jakub Knejzlik on 06/03/15.
+//  Copyright (c) 2015 Jakub Knejzlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GNGeocoder/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GNGeocoder
+//
+//  Created by Jakub Knejzlik on 06/03/15.
+//  Copyright (c) 2015 Jakub Knejzlik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
